ad547bdca43b85d52d9f190911b2738400c00dc2
lpkepka <kepka@pc8.home>
2019-06-30 17:29:50 +0200

    basic UI, parser improved to detect more errors

--- TextCalculation/EquationParser.swift
+++ TextCalculation/EquationParser.swift
@@ -16,56 +16,74 @@ class MathematicalEquationParser {
         let regex = "([0-9.,()+-/*])"
         
         guard equation.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil else {
-            print("Not right characters")
-            completion(nil)
-            return
-        }
-        
-        let openParanthesisCount = equation.filter { $0 == "(" }.count
-        let closeParanthesisCount = equation.filter { $0 == ")" }.count
-        
-        guard openParanthesisCount == closeParanthesisCount else {
-            print("Different number of paranthesis")
+            print("Incorrect characters")
             completion(nil)
             return
         }
         
         let unifiedEquation = equation.replacingOccurrences(of: ",", with: ".").replacingOccurrences(of: " ", with: "")
         
+        guard checkParanthesisOrderAndCount(inEquation: unifiedEquation) else {
+            completion(nil)
+            return
+        }
+        
         recursiveStringCalculation(fromString: unifiedEquation, completion: { result in
             completion(result)
         })
     }
     
+    //MARK: - Main processing method
+    
     private func recursiveStringCalculation(fromString equation: String, completion: (Double?)->()) {
         let bufferedEquation = equation
-        
         //This if else structure provides us with the correct order of executing mathematical operations
         if equation.contains("(") && equation.contains(")") {
             let stringAfterLastOpenParanthesis = equation.split(separator: "(").last!
             let equationWithoutParanthesis = stringAfterLastOpenParanthesis.split(separator: ")").first!
-            recursiveStringCalculation(fromString: String(equationWithoutParanthesis)) { (result) in
-                let newEquation = bufferedEquation.replacingOccurrences(of: "(\(equationWithoutParanthesis))", with: "\(result!)")
+            recursiveStringCalculation(fromString: String(equationWithoutParanthesis)) { (paranthesisResult) in
+                guard paranthesisResult != nil else {
+                    completion(nil)
+                    return
+                }
+                let newEquation = bufferedEquation.replacingOccurrences(of: "(\(equationWithoutParanthesis))", with: "\(paranthesisResult!)")
                 recursiveStringCalculation(fromString: newEquation, completion: completion)
             }
         } else if equation.contains("*") {
             let newEquation = computeOneBlockOfText(ofEquation: equation, withOperation: "*")
-            recursiveStringCalculation(fromString: newEquation, completion: completion)
+            guard let unwrappedNewEquation = newEquation else {
+                completion(nil)
+                return
+            }
+            recursiveStringCalculation(fromString: unwrappedNewEquation, completion: completion)
         } else if equation.contains("/") {
             let newEquation = computeOneBlockOfText(ofEquation: equation, withOperation: "/")
-            recursiveStringCalculation(fromString: newEquation, completion: completion)
-        } else if equation.contains("+") {
-            let newEquation = computeOneBlockOfText(ofEquation: equation, withOperation: "+")
-            recursiveStringCalculation(fromString: newEquation, completion: completion)
+            guard let unwrappedNewEquation = newEquation else {
+                completion(nil)
+                return
+            }
+            recursiveStringCalculation(fromString: unwrappedNewEquation, completion: completion)
         } else if equation.contains("-") && equation.prefix(1) != "-" {
             let newEquation = computeOneBlockOfText(ofEquation: equation, withOperation: "-")
-            recursiveStringCalculation(fromString: newEquation, completion: completion)
+            guard let unwrappedNewEquation = newEquation else {
+                completion(nil)
+                return
+            }
+            recursiveStringCalculation(fromString: unwrappedNewEquation, completion: completion)
+        } else if equation.contains("+") {
+            let newEquation = computeOneBlockOfText(ofEquation: equation, withOperation: "+")
+            guard let unwrappedNewEquation = newEquation else {
+                completion(nil)
+                return
+            }
+            recursiveStringCalculation(fromString: unwrappedNewEquation, completion: completion)
         } else {
             completion(Double(equation))
         }
     }
     
-    func getIndex(forOperation operation: String, inEquation equation: String, searchingForNext isNext: Bool) -> Int {
+    //MARK: - Finding previous/next operations
+    private func getIndex(forOperation operation: String, inEquation equation: String, searchingForNext isNext: Bool) -> Int {
         //Worst case scenario for first index and last index
         let defaultValue = isNext ? equation.count - 1 : 0
         if isNext {
@@ -76,7 +94,7 @@ class MathematicalEquationParser {
     }
     
     
-    private func getIndexOfOperation(fromString equation: String, searchingForNextOperation isNext: Bool) -> Int {
+    private func getIndexOfOperation(fromString equation: String, searchingForNextOperation isNext: Bool) -> Int? {
         let plusIndex = getIndex(forOperation: "+", inEquation: equation, searchingForNext: isNext)
         let minusIndex = getIndex(forOperation: "-", inEquation: equation, searchingForNext: isNext)
         let divisionIndex = getIndex(forOperation: "/", inEquation: equation, searchingForNext: isNext)
@@ -85,7 +103,7 @@ class MathematicalEquationParser {
         let defaultValue = isNext ? equation.count - 1 : 0
         
         guard plusIndex != defaultValue || minusIndex != defaultValue || divisionIndex != defaultValue || multiplicationIndex != defaultValue else {
-            return defaultValue
+            return nil
         }
         
         if isNext {
@@ -95,31 +113,75 @@ class MathematicalEquationParser {
         }
     }
     
-    private func computeOneBlockOfText(ofEquation equation: String, withOperation operation: String) -> String {
+    //MARK: - Processing a simple equation
+    
+    private func computeOneBlockOfText(ofEquation equation: String, withOperation operation: String) -> String? {
+        print(equation)
         let splitEquation = equation.split(separator: operation.first!)
+        
         let indexOfPreviousOperation = getIndexOfOperation(fromString: String(splitEquation[0]), searchingForNextOperation: false)
         let indexOfNextOperation = getIndexOfOperation(fromString: String(splitEquation[1]), searchingForNextOperation: true)
         
-        var firstComponent = splitEquation[0].suffix(splitEquation[0].count - indexOfPreviousOperation-1)
-        if indexOfPreviousOperation == 0 {
-            firstComponent = splitEquation[0]
-        }
+        var firstComponent: String.SubSequence
+        var secondComponent: String.SubSequence
         
-        var secondComponent = splitEquation[1].prefix(indexOfNextOperation)
-        if indexOfNextOperation == splitEquation[1].count - 1 {
+        if indexOfPreviousOperation == nil {
+            //There is no operation in front of the current one
+            firstComponent = splitEquation[0]
+        } else {
+            firstComponent = splitEquation[0].suffix(splitEquation[0].count - indexOfPreviousOperation! - 1)
+        }
+ 
+        if indexOfNextOperation == nil {
+            //There is no operation behind the current one
             secondComponent = splitEquation[1]
+        } else {
+            secondComponent = splitEquation[1].prefix(indexOfNextOperation!)
         }
         
         let result = executeBasicOperation(firstComponent: String(firstComponent),
                                            secondComponent: String(secondComponent),
                                            withOperation: operation)
         
+        guard result != nil else {
+            return nil
+        }
+        
         let range = equation.range(of: firstComponent + operation + secondComponent)!
         let newEquation = equation.replacingCharacters(in: range, with: "\(result!)")
         return newEquation
     }
     
+    //MARK: - Supplementary
     
+    private func checkParanthesisOrderAndCount(inEquation equation: String) -> Bool {
+        var counter = 0
+        
+        //Checking if the paranthesis are in correct order
+        for character in equation {
+            if character == "(" {
+                counter += 1
+            } else if character == ")" {
+                counter -= 1
+            }
+            
+            if counter < 0 {
+                return false
+            }
+        }
+        
+        guard !equation.contains("()") else {
+            return false
+        }
+        
+        //Checking if the number of opening paranthesis is the same as of the closing ones
+        if counter == 0 {
+            return true
+        } else {
+            return false
+        }
+        
+    }
     
     private func executeBasicOperation(firstComponent: String, secondComponent: String, withOperation operation: String) -> Double? {
         
--- TextCalculation/ViewController.swift
+++ TextCalculation/ViewController.swift
@@ -10,12 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Outlets
+    
+    @IBOutlet weak var formulaTextView: UITextView! {
+        didSet {
+            formulaTextView.isEditable = true
+            formulaTextView.keyboardType = .default
+            formulaTextView.layer.borderColor = UIColor.blue.cgColor
+            formulaTextView.layer.borderWidth = 2
+            formulaTextView.layer.cornerRadius = 10
+        }
+    }
+    
+    //MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let equation = "(-3.6+7.2)*(-2.5 + 3.3*(((5.4+5,2))))"
-        MathematicalEquationParser.shared.processEquation(fromString: equation, completion: { result in
-            print(result)
-        })
+        
+    }
+    
+    //MARK: - Actions
+    
+    @IBAction func computeButtonWasPressed(_ sender: UIButton) {
+        if let equation = formulaTextView.text {
+            DispatchQueue.global(qos: .background).async {
+            MathematicalEquationParser.shared.processEquation(fromString: equation, completion: { result in
+                        self.showResultAlert(fromEquation: equation, withResult: result)
+                })
+            }
+        } else {
+            print("Couldn't get formula")
+        }
+    }
+    
+    //MARK: - Alert handling
+    
+    private func showResultAlert(fromEquation equation: String, withResult result: Double?) {
+        var alertText = ""
+        if let computedResult = result {
+            alertText = "The result of \(equation) is \(computedResult)"
+        } else {
+            alertText = "Please make sure Your equation is correct"
+        }
+        
+        let alertController = UIAlertController(title: "Result", message: alertText, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+            alertController.dismiss(animated: true, completion: nil)
+        }
+        alertController.addAction(okAction)
+        DispatchQueue.main.async {
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
 }
 
